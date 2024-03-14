e3c5820ff726e409c189529ced7b4e7b2b807889
EnPei <EnPeiLo@chailease.com.tw>
2024-03-14 15:29:00 +0800

    feat: signal 基本用法了解
    signal一般變數、物件變數、set用法、update用法、computed方法、effect方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-signal-test';
+
+  task$ = new Subject<void>();
+
+  taskCount = signal(0);
+  doubleCount = computed(() => this.taskCount() * 2);
+  effectRef = effect(() => {
+    console.log(`Task count: ${this.taskCount()}`);
+    console.log(`objectSignal: ${this.objectSignal().counter}`);
+  });
+
+  objectSignal = signal({ counter: 0 });
+
+  ngOnInit(): void {
+    this.taskCount.set(1);
+    this.objectSignal.set({ counter: 1 });
+
+    this.task$.subscribe(() => {
+      this.taskCount.update((count) => count + 1);
+
+      this.objectSignal.update((item) => ({
+        ...item,
+        counter: item.counter + 1,
+      }));
+    });
+  }
+
+  onClick() {
+    this.task$.next();
+  }
 }
